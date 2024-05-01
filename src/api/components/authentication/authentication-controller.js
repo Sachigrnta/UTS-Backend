fd349c7f0ea704383409307d5d88608767ea0bde
Sachigrnta <sachigranita26@gmail.com>
2024-05-01 19:44:55 +0700

    Menambah login Attemps Limit

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,5 +1,6 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
+const loginAttempsFail = {};
 
 /**
  * Handle login request
@@ -12,6 +13,31 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    //membuat limit percobaan login sebanyak 5 kali
+    if (loginAttempsFail[email] && loginAttempsFail[email].attempts >= 5) {
+      throw errorResponder(
+        errorTypes.FORBIDDEN,
+        'Too many failed login attempts! Try again after 30 minutes'
+      );
+    }
+    // membuat jangka waktu untuk mencoba login kembali setelah 30 menit
+    if (loginAttempsFail[email]) {
+      const currentTime = Date.now();
+      if (currentTime - loginAttempsFail[email].timeout > 30 * 60 * 1000) {
+        loginAttempsFail[email].attempts = 1; // reset percobaan login menjadi 1 kembali setelah 30 menit timeout
+        loginAttempsFail[email].timeout = currentTime; //cek apabila waktu batas login sudah lebih dari 30 menit
+      } else {
+        // +1 pada percobaan login yang gagal, bila mencoba < 30 min dari waktu timeout
+        loginAttempsFail[email].attempts++;
+      }
+    } //untuk memproses percobaan login alamat email baru
+    else {
+      loginAttempsFail[email] = {
+        attempts: 1,
+        timeout: Date.now(),
+      };
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -24,6 +50,9 @@ async function login(request, response, next) {
         'Wrong email or password'
       );
     }
+
+    // delete loginAttempFail apabila login berhasil.
+    delete loginAttempsFail[email];
 
     return response.status(200).json(loginSuccess);
   } catch (error) {
