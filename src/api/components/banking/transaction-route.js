23cf02394f2174a03449de753edae6ddb7df0d28
Sachigrnta <sachigranita26@gmail.com>
2024-05-05 23:22:48 +0700

    create transaction route file

---
+++ src/api/components/banking/transaction-route.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const transactionControllers = require('./transaction-controller');
+const transactionValidator = require('./transaction-validator');
+const authenticationMiddleware = require('../../middlewares/authentication-middleware');
+const celebrate = require('../../../core/celebrate-wrappers');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/transactions', route);
+
+  // Create transaction
+  route.post(
+    '/',
+    authenticationMiddleware,
+    celebrate(transactionValidator.createTransactionValidator),
+    transactionControllers.createTransaction
+  );
+
+  // Get transaction detail
+  route.get(
+    '/:id',
+    authenticationMiddleware,
+    transactionControllers.getTransaction
+  );
+
+  // Update transaction
+  route.put(
+    '/:id',
+    authenticationMiddleware,
+    celebrate(transactionValidator.updateTransactionValidator),
+    transactionControllers.updateTransaction
+  );
+
+  // Delete transaction
+  route.delete(
+    '/:id',
+    authenticationMiddleware,
+    transactionControllers.deleteTransaction
+  );
+};
